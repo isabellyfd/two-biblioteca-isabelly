aad23361d8d493f973276e70938843ce870edbec
Isabelly <icfd@cin.ufpe.br>
2018-01-04 11:48:12 -0300

    [Red Bar] Getting all books in library failed

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,14 +1,25 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.entity.Book;
 import com.twu.biblioteca.util.ConsoleHelper;
 
+import java.util.ArrayList;
+
 public class BibliotecaApp {
+
+    public BibliotecaApp(ArrayList<Book> books){
+
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
 
-    protected void printWelcomeMessage(){
+    public void printWelcomeMessage(){
         System.out.print(ConsoleHelper.welcomeMessage());
+    }
+
+    protected ArrayList<Book> getBooksInLibrary() {
+        return new ArrayList<>();
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.entity.Book;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -7,6 +8,7 @@ import org.junit.Assert;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 public class BibliotecaAppTest {
 
@@ -20,7 +22,11 @@ public class BibliotecaAppTest {
 
     @Before
     public void setup(){
-        app = new BibliotecaApp();
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book("Goodnight Moon", "Margaret Wise Brown, Clement Hurd", 2007));
+        books.add(new Book("Press Here", "Herve Tullet", 2011));
+
+        app = new BibliotecaApp(books);
     }
 
 
@@ -28,5 +34,20 @@ public class BibliotecaAppTest {
     public void testIfPrintedWelcomeMessageWhenCalledPrintWelcomeMethod() {
         app.printWelcomeMessage();
         Assert.assertEquals("Hello, explorer!\nWelcome to Isabelly's awesome library!\nI hope you enjoy your stay!\n", outContent.toString());
+    }
+
+    @Test
+    public void testIfAllBookWereRecovered(){
+        ArrayList<Book> booksInLibrary = app.getBooksInLibrary();
+
+        Assert.assertNotNull(booksInLibrary);
+        Assert.assertEquals(2, booksInLibrary.size());
+        Assert.assertEquals("Goodnight Moon", booksInLibrary.get(0).getName());
+        Assert.assertEquals("Margaret Wise Brown, Clement Hurd", booksInLibrary.get(0).getAuthor());
+        Assert.assertEquals(2007, booksInLibrary.get(0).getPublishYear());
+
+        Assert.assertEquals("Press Here", booksInLibrary.get(1).getName());
+        Assert.assertEquals("Herve Tullet", booksInLibrary.get(1).getAuthor());
+        Assert.assertEquals(2011, booksInLibrary.get(1).getPublishYear());
     }
 }
