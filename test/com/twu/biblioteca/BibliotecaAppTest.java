460651fe318f46f87ad5f17d14c62ccf3460e0f8
Isabelly <icfd@cin.ufpe.br>
2018-01-04 11:14:30 -0300

    [Red Bar] test to see if welcome print is on console failed

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,4 +1,32 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class BibliotecaAppTest {
+
+    private static final ByteArrayOutputStream outContent =  new ByteArrayOutputStream();
+    private BibliotecaApp app;
+
+    @BeforeClass
+    public static void setupStreams(){
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Before
+    public void setup(){
+        app = new BibliotecaApp();
+    }
+
+
+    @Test
+    public void testIfPrintedWelcomeMessageWhenCalledPrintWelcomeMethod() {
+        app.printWelcomeMessage();
+        Assert.assertEquals("Hello, explorer!\nWelcome to Isabelly's awesome library!\nI hope you enjoy your stay!", outContent.toString());
+    }
 }
