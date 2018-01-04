37ee40716b85aaaa58dda95b51522400e5126d57
Isabelly <icfd@cin.ufpe.br>
2018-01-04 10:58:05 -0300

    [Red Bar] Test welcome message failed!

---
+++ src/com/twu/biblioteca/util/ConsoleHelper.java
@@ -0,0 +1,8 @@
+package com.twu.biblioteca.util;
+
+public class ConsoleHelper {
+
+    public static String welcomeMessage() {
+        return "";
+    }
+}
--- test/com/twu/biblioteca/util/ConsoleHelperTest.java
+++ test/com/twu/biblioteca/util/ConsoleHelperTest.java
@@ -1,5 +1,13 @@
 package com.twu.biblioteca.util;
 
+import org.junit.Test;
+import org.junit.Assert;
+import com.twu.biblioteca.util.ConsoleHelper;
 
 public class ConsoleHelperTest {
+
+    @Test
+    public void testPrintWelcomeMessageCorrectly() {
+        Assert.assertEquals("Hello, explorer!\nWelcome to Isabelly's awesome library!\nI hope you enjoy your stay!", ConsoleHelper.welcomeMessage());
+    }
 }
