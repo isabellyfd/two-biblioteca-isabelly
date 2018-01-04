fe189413315e8a199d911e9d935bf256b103780d
Isabelly <icfd@cin.ufpe.br>
2018-01-04 10:50:47 -0300

    [Refactor] changing static import to import class

--- test/com/twu/biblioteca/entity/BookTest.java
+++ test/com/twu/biblioteca/entity/BookTest.java
@@ -3,7 +3,7 @@ package com.twu.biblioteca.entity;
 import org.junit.Test;
 
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
 
 public class BookTest {
 
@@ -12,24 +12,24 @@ public class BookTest {
     public void testBookAttributesEquality() {
 
         Book book = new Book("O pequeno príncipe", "Le vie es rouge" , 2001);
-        assertEquals("O pequeno príncipe", book.getName());
-        assertEquals("Le vie es rouge", book.getAuthor());
-        assertEquals(2001, book.getPublishYear());
-        assertEquals(true, book.isAvailable());
+        Assert.assertEquals("O pequeno príncipe", book.getName());
+        Assert.assertEquals("Le vie es rouge", book.getAuthor());
+        Assert.assertEquals(2001, book.getPublishYear());
+        Assert.assertEquals(true, book.isAvailable());
     }
 
     @Test
     public void testIsAvailableDidChangeTheValueToFalse() {
         Book book = new Book("O pequeno príncipe", "Le vie es rouge" , 2001);
         book.setAvailable(false);
-        assertEquals(false, book.isAvailable());
+        Assert.assertEquals(false, book.isAvailable());
     }
 
     @Test
     public void testIfABookIsEqualsToItself(){
         Book book = new Book("O pequeno príncipe", "Le vie es rouge" , 2001);
         Book sameBook = new Book("O pequeno príncipe", "Le vie es rouge" , 2001);
-        assertEquals(true, book.equals(sameBook));
+        Assert.assertEquals(true, book.equals(sameBook));
     }
 
 
