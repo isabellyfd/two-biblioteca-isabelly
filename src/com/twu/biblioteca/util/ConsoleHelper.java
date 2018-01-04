e96b0dbfa555999865b7962f2b63d534a3c8afc5
Isabelly <icfd@cin.ufpe.br>
2018-01-04 11:20:35 -0300

    [Green Bar] added \n to the string in ConsoleHelper.
    There is no need to refactor

--- src/com/twu/biblioteca/util/ConsoleHelper.java
+++ src/com/twu/biblioteca/util/ConsoleHelper.java
@@ -1,7 +1,7 @@
 package com.twu.biblioteca.util;
 
 public class ConsoleHelper {
-    private static final String WELCOME_MESSAGE = "Hello, explorer!\nWelcome to Isabelly's awesome library!\nI hope you enjoy your stay!";
+    private static final String WELCOME_MESSAGE = "Hello, explorer!\nWelcome to Isabelly's awesome library!\nI hope you enjoy your stay!\n";
 
     public static String welcomeMessage() {
         return WELCOME_MESSAGE;
