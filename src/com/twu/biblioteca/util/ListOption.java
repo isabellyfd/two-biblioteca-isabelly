9caf58aea387108d198c9e23c36c4ea2c851b801
Isabelly <icfd@cin.ufpe.br>
2018-01-04 15:31:20 -0300

    [Refactor] eliminating hard code value

--- src/com/twu/biblioteca/util/ListOption.java
+++ src/com/twu/biblioteca/util/ListOption.java
@@ -3,13 +3,14 @@ package com.twu.biblioteca.util;
 public class ListOption {
 
     private final String COMMAND = "list";
+    private final String MESSAGE = "[list] shows you all avaliable books in the library";
 
     public boolean check(String userInput){
         return COMMAND.equals(userInput);
     }
 
     public String getMessage(){
-        return "[list] shows you all avaliable books in the library";
+        return MESSAGE;
     }
 
 }
