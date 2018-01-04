7fe0f7d1c4fed54f6247269b70466e12f2abe958
Isabelly <icfd@cin.ufpe.br>
2018-01-04 12:18:55 -0300

    [Refactor / Red Bar] To refactor the program, it initialize an test to print all elements in a array list  as toString

--- src/com/twu/biblioteca/util/ConsoleHelper.java
+++ src/com/twu/biblioteca/util/ConsoleHelper.java
@@ -1,9 +1,18 @@
 package com.twu.biblioteca.util;
 
+import com.twu.biblioteca.entity.Book;
+
+import java.util.ArrayList;
+
 public class ConsoleHelper {
+
     private static final String WELCOME_MESSAGE = "Hello, explorer!\nWelcome to Isabelly's awesome library!\nI hope you enjoy your stay!\n";
 
     public static String welcomeMessage() {
         return WELCOME_MESSAGE;
+    }
+
+    public static void printList(ArrayList<Book> books){
+
     }
 }
--- test/com/twu/biblioteca/util/ConsoleHelperTest.java
+++ test/com/twu/biblioteca/util/ConsoleHelperTest.java
@@ -1,13 +1,40 @@
 package com.twu.biblioteca.util;
 
+import com.twu.biblioteca.entity.Book;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.Assert;
 import com.twu.biblioteca.util.ConsoleHelper;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
 public class ConsoleHelperTest {
+
+    private static final ByteArrayOutputStream outContent =  new ByteArrayOutputStream();
+
+    @BeforeClass
+    public static void setupStreams(){
+        System.setOut(new PrintStream(outContent));
+    }
+
+
 
     @Test
     public void testPrintWelcomeMessageCorrectly() {
         Assert.assertEquals("Hello, explorer!\nWelcome to Isabelly's awesome library!\nI hope you enjoy your stay!\n", ConsoleHelper.welcomeMessage());
+    }
+
+    @Test
+    public void testPrintAllElementsOfTheListFormattedWithToString() {
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book("Goodnight Moon", "Margaret Wise Brown, Clement Hurd", 2007));
+        books.add(new Book("Press Here", "Herve Tullet", 2011));
+
+        ConsoleHelper.printList(books);
+        Assert.assertEquals("1 - Goodnight Moon by Margaret Wise Brown, Clement Hurd - 2007", outContent.toString().trim());
+        Assert.assertEquals("2 - Press Here by Herve Tullet - 2011", outContent.toString().trim());
     }
 }
