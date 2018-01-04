12a42b31e9e5d755d20fc50ea66b53a3a290495f
Isabelly <icfd@cin.ufpe.br>
2018-01-04 11:12:59 -0300

    [Green Bar] Test welcome passed!

--- src/com/twu/biblioteca/util/ConsoleHelper.java
+++ src/com/twu/biblioteca/util/ConsoleHelper.java
@@ -2,7 +2,8 @@ package com.twu.biblioteca.util;
 
 public class ConsoleHelper {
 
+
     public static String welcomeMessage() {
-        return "";
+        return "Hello, explorer!\nWelcome to Isabelly's awesome library!\nI hope you enjoy your stay!";
     }
 }
