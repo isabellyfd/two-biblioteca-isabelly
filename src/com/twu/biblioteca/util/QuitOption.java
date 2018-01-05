14e58ee0dc49f58eff8807cbc667a01c264057df
Isabelly <icfd@cin.ufpe.br>
2018-01-05 10:55:46 -0300

    [REFACTOR / Feature] creating a constant to eliminate raw value in the code

--- src/com/twu/biblioteca/util/QuitOption.java
+++ src/com/twu/biblioteca/util/QuitOption.java
@@ -3,12 +3,13 @@ package com.twu.biblioteca.util;
 public class QuitOption {
 
     private final String COMMAND = "quit";
+    private final String MESSAGE = "[quit] quits the program";
 
     public boolean check(String userInput){
         return COMMAND.equals(userInput);
     }
 
     public String getMessage() {
-        return "[quit] quits the program";
+        return MESSAGE;
     }
 }
