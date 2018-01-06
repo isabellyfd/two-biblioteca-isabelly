b8099b37d08493efac9f5e8bfb3b0ca447d04d55
Isabelly <icfd@cin.ufpe.br>
2018-01-06 20:05:24 -0300

    [RED BAR / TEST] Created CheckoutOption menu

---
+++ src/com/twu/biblioteca/menu/CheckoutOption.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca.menu;
+
+public class CheckoutOption extends Option {
+
+    public CheckoutOption() {
+    }
+
+    @Override
+    public void action() {
+
+    }
+}
--- test/com/twu/biblioteca/menu/CheckoutOptionTest.java
+++ test/com/twu/biblioteca/menu/CheckoutOptionTest.java
@@ -1,6 +1,15 @@
 package com.twu.biblioteca.menu;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CheckoutOptionTest {
 
-
+    @Test
+    public void testIfCheckOutOptionIsChosen() {
+        String userInput =  "checkout";
+        CheckoutOption checkoutOption = new CheckoutOption();
+        Assert.assertTrue(checkoutOption.check(userInput));
+    }
 }
