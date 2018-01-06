5b4c38a28671ee1e391fed42970b3386f280334b
Isabelly <icfd@cin.ufpe.br>
2018-01-06 19:39:06 -0300

    adding action method to quit  option

--- src/com/twu/biblioteca/menu/QuitOption.java
+++ src/com/twu/biblioteca/menu/QuitOption.java
@@ -1,9 +1,14 @@
 package com.twu.biblioteca.menu;
 
 public class QuitOption extends Option {
+    private final int EXIT_WITHOUT_ERRORS = 0;
 
     public QuitOption(){
         this.setCommand(Commands.QUIT_COMMAND);
         this.setMessage(Commands.QUIT_COMMAND_MESSAGE);
+    }
+
+    public void action() {
+        System.exit(EXIT_WITHOUT_ERRORS);
     }
 }
