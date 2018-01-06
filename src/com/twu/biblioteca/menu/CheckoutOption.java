7399e1fd7d567c97496f59b1f6695e0400725340
Isabelly <icfd@cin.ufpe.br>
2018-01-06 20:19:38 -0300

    [GREEN BAR / FEATURED] setting message to expected

--- src/com/twu/biblioteca/menu/CheckoutOption.java
+++ src/com/twu/biblioteca/menu/CheckoutOption.java
@@ -4,6 +4,7 @@ public class CheckoutOption extends Option {
 
     public CheckoutOption() {
         this.setCommand("checkout");
+        this.setMessage("[checkout] <index> checks out the book in the index passed as argument.");
     }
 
     @Override
