18c0fd50cc096c9a769af5fbec82d6abc84825a0
Isabelly <icfd@cin.ufpe.br>
2018-01-06 21:35:26 -0300

    [REFACTOR] adding a eqaulity checker with parameter

--- src/com/twu/biblioteca/menu/CheckoutOption.java
+++ src/com/twu/biblioteca/menu/CheckoutOption.java
@@ -23,7 +23,7 @@ public class CheckoutOption extends Option {
     }
 
     protected boolean isPositiveDecision(String decision){
-        return true;
+        return "yes".equals(decision);
     }
 
 }
