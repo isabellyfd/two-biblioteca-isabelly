eedc4a5d1db4e0374e9530b296685e76f2340861
Isabelly <icfd@cin.ufpe.br>
2018-01-08 16:56:45 -0300

    [RED BAR] adding movie list option

---
+++ src/com/twu/biblioteca/menu/ListMoviesOption.java
@@ -0,0 +1,9 @@
+package com.twu.biblioteca.menu;
+
+public class ListMoviesOption extends Option {
+
+    @Override
+    public void action(int index) {
+        System.out.println("list movies");
+    }
+}
---
+++ test/com/twu/biblioteca/menu/ListMoviesOptionTest.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca.menu;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ListMoviesOptionTest {
+
+    @Test
+    public void testIfUserChoiceWasListMovies() {
+        String userInput = "list-movies";
+        ListMoviesOption listMoviesOption =  new ListMoviesOption();
+        Assert.assertTrue(listMoviesOption.check(userInput));
+    }
+}
