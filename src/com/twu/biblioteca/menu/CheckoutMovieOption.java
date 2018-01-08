2ee4b86e87f86e767cbe88566b5ceb13477a2656
Isabelly <icfd@cin.ufpe.br>
2018-01-08 19:21:28 -0300

    [GREEN BAR] setting value in message

--- src/com/twu/biblioteca/menu/CheckoutMovieOption.java
+++ src/com/twu/biblioteca/menu/CheckoutMovieOption.java
@@ -4,6 +4,7 @@ public class CheckoutMovieOption extends Option {
 
     public CheckoutMovieOption() {
         this.setCommand(Commands.CHECKOUT_MOVIE_COMMAND);
+        this.setMessage("[checkout-movie] <index> checks out the movie in the index passed as argument");
     }
 
     @Override
