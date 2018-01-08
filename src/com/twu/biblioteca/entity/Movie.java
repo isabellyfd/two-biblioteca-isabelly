a0ff5e6e38af96c772b90817a837cf67f3ec0b8b
Isabelly <icfd@cin.ufpe.br>
2018-01-08 17:40:01 -0300

    [RED BAR] testing creation of movies

---
+++ src/com/twu/biblioteca/entity/Movie.java
@@ -0,0 +1,24 @@
+package com.twu.biblioteca.entity;
+
+public class Movie {
+
+    public Movie(String name, String director, int year, double rating){
+
+    }
+
+    public String getName() {
+        return "";
+    }
+
+    public String getDirector(){
+        return "";
+    }
+
+    public int getYear() {
+        return 1;
+    }
+
+    public double getRating() {
+        return 1.0;
+    }
+}
---
+++ test/com/twu/biblioteca/entity/MovieTest.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca.entity;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MovieTest {
+
+    @Test
+    public void testMovieCreation() {
+        Movie movie = new Movie("Little Miss Sunshine", "Jonathan Dayton, Valerie Faris", 2006, 5.0);
+        Assert.assertEquals("Little Miss Sunshine", movie.getName());
+        Assert.assertEquals("Jonathan Dayton, Valerie Faris", movie.getDirector());
+        Assert.assertEquals(2006, movie.getYear());
+        Assert.assertEquals(5.0, movie.getRating());
+    }
+}
