65073a191cd34b7f6e2ce46310dab2bc973a3b82
Isabelly <icfd@cin.ufpe.br>
2018-01-08 19:28:05 -0300

    [REFACTOR] renaming test class to CheckoutBookOptionTest

--- test/com/twu/biblioteca/menu/CheckoutBookOptionTest.java
+++ test/com/twu/biblioteca/menu/CheckoutBookOptionTest.java
@@ -5,7 +5,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CheckoutOptionTest {
+public class CheckoutBookOptionTest {
 
     private CheckoutBookOption checkoutOption;
     @Before
