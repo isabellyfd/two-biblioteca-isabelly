bfa5d1b25dc43e608c298084b30c288dcf3879fe
Isabelly <icfd@cin.ufpe.br>
2018-01-08 19:23:02 -0300

    [REFACTOR] Eliminate raw value in class

--- src/com/twu/biblioteca/menu/CheckoutMovieOption.java
+++ src/com/twu/biblioteca/menu/CheckoutMovieOption.java
@@ -4,7 +4,7 @@ public class CheckoutMovieOption extends Option {
 
     public CheckoutMovieOption() {
         this.setCommand(Commands.CHECKOUT_MOVIE_COMMAND);
-        this.setMessage("[checkout-movie] <index> checks out the movie in the index passed as argument");
+        this.setMessage(Commands.CHECKOUT_MOVIE_COMMAND_MESSAGE);
     }
 
     @Override
--- src/com/twu/biblioteca/menu/Commands.java
+++ src/com/twu/biblioteca/menu/Commands.java
@@ -13,4 +13,5 @@ public class Commands {
     public static final String CHECKOUT_COMMAND_MESSAGE = "[checkout] <index> checks out the book in the index passed as argument";
     public static final String RETURN_COMMAND_MESSAGE = "[return] returns a book to library";
     public static final String LIST_MOVIES_COMMAND_MESSAGE = "[list-movies] list all available movies in the library";
+    public static final String CHECKOUT_MOVIE_COMMAND_MESSAGE = "[checkout-movie] <index> checks out the movie in the index passed as argument";
 }
