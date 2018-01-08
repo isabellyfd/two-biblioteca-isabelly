fa046cf1b06c6e8db8cb230627a97cab60baeabd
Isabelly <icfd@cin.ufpe.br>
2018-01-08 11:23:57 -0300

    [GREEN BAR] making funtion of check test pass

--- src/com/twu/biblioteca/menu/ReturnOption.java
+++ src/com/twu/biblioteca/menu/ReturnOption.java
@@ -2,6 +2,10 @@ package com.twu.biblioteca.menu;
 
 public class ReturnOption extends Option {
 
+    public ReturnOption(){
+        this.setCommand("return");
+    }
+
     @Override
     public void action(int index) {
 
