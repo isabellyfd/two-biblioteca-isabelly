4fc7a05e582dbd933ca8634a48a046580ce28e3a
Isabelly <icfd@cin.ufpe.br>
2018-01-08 12:44:21 -0300

    [GREEN BAR] making the test pass setting the expected message

--- src/com/twu/biblioteca/menu/ReturnOption.java
+++ src/com/twu/biblioteca/menu/ReturnOption.java
@@ -4,6 +4,7 @@ public class ReturnOption extends Option {
 
     public ReturnOption(){
         this.setCommand(Commands.RETURN_COMMAND);
+        this.setMessage("[return] returns a book to library");
     }
 
     @Override
