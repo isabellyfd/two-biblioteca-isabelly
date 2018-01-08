0c04b3122008180e9104f382c0a194af9fc17661
Isabelly <icfd@cin.ufpe.br>
2018-01-08 19:12:39 -0300

    [RED BAR] adding test to check command

---
+++ src/com/twu/biblioteca/menu/CheckoutMovieOption.java
@@ -0,0 +1,8 @@
+package com.twu.biblioteca.menu;
+
+public class CheckoutMovieOption extends Option {
+    @Override
+    public void action(int index) {
+        
+    }
+}
--- test/com/twu/biblioteca/menu/CheckoutMovieOptionTest.java
+++ test/com/twu/biblioteca/menu/CheckoutMovieOptionTest.java
@@ -1,6 +1,14 @@
 package com.twu.biblioteca.menu;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CheckoutMovieOptionTest {
 
-    
+    @Test
+    public void testIfCheckOutMovieIsChosen() {
+        String userInput = "checkout-movie";
+        CheckoutMovieOption checkoutMovieOption = new CheckoutMovieOption();
+        Assert.assertTrue(checkoutMovieOption.check(userInput));
+    }
 }
