e2c284a4381d772ab79016e772e4239402ca8e49
Isabelly <icfd@cin.ufpe.br>
2018-01-08 17:14:23 -0300

    [REFACTOR / TEST] adding setup in ListMoviesTest class

--- test/com/twu/biblioteca/menu/ListMoviesOptionTest.java
+++ test/com/twu/biblioteca/menu/ListMoviesOptionTest.java
@@ -1,27 +1,33 @@
 package com.twu.biblioteca.menu;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ListMoviesOptionTest {
 
+    private ListMoviesOption listMoviesOption;
+
+    @Before
+    public void setup() {
+        this.listMoviesOption = new ListMoviesOption();
+    }
+
     @Test
     public void testIfUserChoiceWasListMovies() {
         String userInput = "list-movies";
-        ListMoviesOption listMoviesOption =  new ListMoviesOption();
         Assert.assertTrue(listMoviesOption.check(userInput));
     }
 
     @Test
     public void testIfUserChoiceWasNotListMovies() {
         String userInput = "list-movie";
-        ListMoviesOption listMoviesOption = new ListMoviesOption();
         Assert.assertFalse(listMoviesOption.check(userInput));
     }
 
     @Test
     public void testGettingMessageFromListMovieOption() {
-        ListMoviesOption listMoviesOption = new ListMoviesOption();
+        
         Assert.assertEquals("[list-movies] list all movies available in the library", listMoviesOption.getMessage());
     }
 
