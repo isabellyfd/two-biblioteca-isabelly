e279ec7bfaa21ea5d2bee3f315f7b1987c2224cf
Isabelly <icfd@cin.ufpe.br>
2018-01-08 19:13:30 -0300

    [GREEN BAR] setting command

--- src/com/twu/biblioteca/menu/CheckoutMovieOption.java
+++ src/com/twu/biblioteca/menu/CheckoutMovieOption.java
@@ -1,8 +1,13 @@
 package com.twu.biblioteca.menu;
 
 public class CheckoutMovieOption extends Option {
+
+    public CheckoutMovieOption() {
+        this.setCommand("checkout-movie");
+    }
+
     @Override
     public void action(int index) {
-        
+
     }
 }
