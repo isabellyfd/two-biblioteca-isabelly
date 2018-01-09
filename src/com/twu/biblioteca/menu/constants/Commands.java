d22e93ab220faa0c812c443f6096415196e1263c
Isabelly <icfd@cin.ufpe.br>
2018-01-09 10:16:19 -0300

    [green bar] changing command and message

--- src/com/twu/biblioteca/menu/constants/Commands.java
+++ src/com/twu/biblioteca/menu/constants/Commands.java
@@ -4,14 +4,14 @@ public class Commands {
     public static final String QUIT_COMMAND = "quit";
     public static final String LIST_BOOKS_COMMAND = "list-books";
     public static final String CHECKOUT_BOOK_COMMAND = "checkout";
-    public static final String RETURN_COMMAND = "return";
+    public static final String RETURN_COMMAND = "return-book";
     public static final String LIST_MOVIES_COMMAND = "list-movies";
     public static final String CHECKOUT_MOVIE_COMMAND = "checkout-movie";
 
     public static final String QUIT_COMMAND_MESSAGE = "[quit] quits the program";
     public static final String LIST_BOOKS_COMMAND_MESSAGE = "[list-books] shows you all available books in the library";
     public static final String CHECKOUT_BOOK_COMMAND_MESSAGE = "[checkout-book] <index> checks out the book in the index passed as argument";
-    public static final String RETURN_COMMAND_MESSAGE = "[return] returns a book to library";
+    public static final String RETURN_COMMAND_MESSAGE = "[return-book] returns a book to library";
     public static final String LIST_MOVIES_COMMAND_MESSAGE = "[list-movies] list all available movies in the library";
     public static final String CHECKOUT_MOVIE_COMMAND_MESSAGE = "[checkout-movie] <index> checks out the movie in the index passed as argument";
 }
