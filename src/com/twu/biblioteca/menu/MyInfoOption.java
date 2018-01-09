bc6450c5c2d99e7580c41a9aff34aafdbadd31b3
Isabelly <icfd@cin.ufpe.br>
2018-01-09 14:03:36 -0300

    [red bar] creatin myinfo option

---
+++ src/com/twu/biblioteca/menu/MyInfoOption.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca.menu;
+
+import com.twu.biblioteca.entity.User;
+
+public class MyInfoOption extends Option {
+
+    MyInfoOption(User user){
+
+    }
+    @Override
+    public void action(int index) {
+
+    }
+}
---
+++ test/com/twu/biblioteca/menu/MyInfoOptionTest.java
@@ -0,0 +1,17 @@
+package com.twu.biblioteca.menu;
+
+import com.twu.biblioteca.entity.User;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyInfoOptionTest {
+
+
+    @Test
+    public void testIfMyInfoOptionIsChosen(){
+        String userInput = "my-info";
+        User myUser = new User("Isabelly Damascena", "icfd@example.com", "(81) 998061240", "000-0001", "bellypass");
+        MyInfoOption myInfoOption = new MyInfoOption(myUser);
+        Assert.assertTrue(myInfoOption.check(userInput));
+    }
+}
