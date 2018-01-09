5be0b0c95329f093e60c23c799d52be68ded2a33
Isabelly <icfd@cin.ufpe.br>
2018-01-09 13:52:15 -0300

    [green bar] adding message

--- src/com/twu/biblioteca/exception/CouldNotLoginException.java
+++ src/com/twu/biblioteca/exception/CouldNotLoginException.java
@@ -1,4 +1,8 @@
 package com.twu.biblioteca.exception;
 
 public class CouldNotLoginException  extends Exception{
+
+    public CouldNotLoginException() {
+        super("Password or library number incorrect! try again!");
+    }
 }
