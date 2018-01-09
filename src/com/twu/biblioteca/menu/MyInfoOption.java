19bb748cb12410019a824fdede5347a39c7649e7
Isabelly <icfd@cin.ufpe.br>
2018-01-09 14:31:35 -0300

    [build] buildign feature 90%

--- src/com/twu/biblioteca/menu/MyInfoOption.java
+++ src/com/twu/biblioteca/menu/MyInfoOption.java
@@ -2,16 +2,20 @@ package com.twu.biblioteca.menu;
 
 import com.twu.biblioteca.entity.User;
 import com.twu.biblioteca.menu.constants.Commands;
+import com.twu.biblioteca.util.ConsoleHelper;
 
 public class MyInfoOption extends Option {
+
+    private User me;
 
     MyInfoOption(User user){
         this.setCommand(Commands.MY_INFO_COMMAND);
         this.setMessage(Commands.MY_INFO_COMMAND_MESSAGE);
+        this.me = user;
     }
 
     @Override
     public void action(int index) {
-
+        ConsoleHelper.shared.printMessage(this.me.toString());
     }
 }
