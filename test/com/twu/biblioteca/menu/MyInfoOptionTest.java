2cf7eb6d4c423cee985d64c4a657e4e57a5e89c3
Isabelly <icfd@cin.ufpe.br>
2018-01-09 14:12:43 -0300

    [red bar] for getting message

--- test/com/twu/biblioteca/menu/MyInfoOptionTest.java
+++ test/com/twu/biblioteca/menu/MyInfoOptionTest.java
@@ -2,15 +2,26 @@ package com.twu.biblioteca.menu;
 
 import com.twu.biblioteca.entity.User;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MyInfoOptionTest {
 
+    private MyInfoOption myInfoOption;
+    @Before
+    public void setup() {
+        User myUser = new User("Isabelly Damascena", "icfd@example.com", "(81) 998061240", "000-0001", "bellypass");
+        this.myInfoOption = new MyInfoOption(myUser);
+    }
+
     @Test
     public void testIfMyInfoOptionIsChosen(){
         String userInput = "my-info";
-        User myUser = new User("Isabelly Damascena", "icfd@example.com", "(81) 998061240", "000-0001", "bellypass");
-        MyInfoOption myInfoOption = new MyInfoOption(myUser);
         Assert.assertTrue(myInfoOption.check(userInput));
+    }
+
+    @Test
+    public void testGettingMyInfoOptionCommand() {
+        Assert.assertEquals("[my-info] show all information about you", myInfoOption.getMessage());
     }
 }
