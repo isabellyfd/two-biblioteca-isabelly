2bb01c3eb58a1a663cdc98d7f16c5a1e264fe0bc
Isabelly <icfd@cin.ufpe.br>
2018-01-09 14:09:38 -0300

    [refactor] eliminating raw value

--- src/com/twu/biblioteca/menu/MyInfoOption.java
+++ src/com/twu/biblioteca/menu/MyInfoOption.java
@@ -1,11 +1,12 @@
 package com.twu.biblioteca.menu;
 
 import com.twu.biblioteca.entity.User;
+import com.twu.biblioteca.menu.constants.Commands;
 
 public class MyInfoOption extends Option {
 
     MyInfoOption(User user){
-        this.setCommand("my-info");
+        this.setCommand(Commands.MY_INFO_COMMAND);
     }
 
     @Override
--- src/com/twu/biblioteca/menu/constants/Commands.java
+++ src/com/twu/biblioteca/menu/constants/Commands.java
@@ -7,6 +7,7 @@ public class Commands {
     public static final String RETURN_COMMAND = "return-book";
     public static final String LIST_MOVIES_COMMAND = "list-movies";
     public static final String CHECKOUT_MOVIE_COMMAND = "checkout-movie";
+    public static final String MY_INFO_COMMAND = "my-info";
 
     public static final String QUIT_COMMAND_MESSAGE = "[quit] quits the program";
     public static final String LIST_BOOKS_COMMAND_MESSAGE = "[list-books] shows you all available books in the library";
